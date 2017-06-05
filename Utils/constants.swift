5b9076aaffce490ee7cd96d0b1a39c51acee21b9
sgolds <sgolds@sandiego.edu>
2017-06-04 19:31:16 -0700

    Constants

---
+++ Utils/constants.swift
@@ -0,0 +1,22 @@
+//
+//  constants.swift
+//  Writing App
+//
+//  Created by Sten Golds on 1/7/17.
+//  Copyright © 2017 Sten Golds. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//colors
+let commonMaterialGreen = UIColor(red: 0.0/255.0, green: 150.0/255.0, blue: 136.0/255.0, alpha: 1.0)
+let blueBlackFont = UIColor(red: 66.0/255.0, green: 66.0/255.0, blue: 66.0/255.0, alpha: 1.0)
+let highlightGreen = UIColor(red: 0.0/255.0, green: 121.0/255.0, blue: 107.0/255.0, alpha: 1.0)
+
+
+//keys
+let FONT_SIZE_KEY = "user font preference"
+let TIME_KEY = "user time preference"
+
+
